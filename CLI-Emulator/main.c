bcfcc0e1ff5ecf2de56cf01c5944cc8ee3c28a4c
evans-prince <princeevans2004@gmail.com>
2025-06-08 15:16:03 +0530

    WIP: main function written

--- CLI-Emulator/main.c
+++ CLI-Emulator/main.c
@@ -88,9 +88,186 @@ struct label *labels;
 
 
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(int argc, const char * argv[])
+{
+    
+    k = 1;
+    
+    //Code for opening the file and computing the size of the program
+    long int size;
+    FILE* f = fopen(argv[1], "r");        // argv[1] is the name of the file in which the program is written
+    if(f == 0)
+    {
+        printf("Could not read file!!!\n");
+        exit(0);
+    }
+    else
+    {
+        fseek(f, 0, SEEK_END);    // fseek takes f to EOF
+        size = ftell(f);        // ftell gives the position of f which is EOF and hence returns the length of the file
+    }
+    size = size + 2;            // We add 2 to accomodate for an extra '\n' and a '\0'
+    
+    
+    
+    // Code for storing the whole program into the string str
+    str = malloc(size*(sizeof(char)));
+    rewind(f);                    // Brings f to the beginning of the file
+    int colon_count = 0;        // colon_count will contain the number of colons in the program which decides the maximum number of labels
+    int line_count = 0;            // line_count will contain the number of lines in the program which decides the maximum number of instructions
+    int i = 0;
+    int x = fgetc(f);
+    while(x != EOF)
+    {
+        if(x != 13)         // We neglect the carriage returns '/r' (ASCII 13) if present
+        {
+            if(x == '/')        // We neglect the multiline comments and donot add them to the string str
+            {
+                x = fgetc(f);
+                if(x == '*')
+                {
+                    while(1)
+                    {
+                        x = fgetc(f);
+                        if(x == EOF )
+                        {
+                            printf("Error: Unterminated Comment!\n");
+                            exit(0);
+                        }
+                        else if (x == '*')
+                        {
+                            x = fgetc(f);
+                            if( x == '/')
+                            {
+                                x=fgetc(f);
+                                break;
+                            }
+                        }
+                    }
+                }
+                else if(x == EOF)
+                {
+                    str[i++] = '/';
+                    break;
+                }
+                else
+                    str[i++] = '/';
+            }
+            if(x == EOF)
+                break;
+            str[i++] = x;
+            if(x == '\n')
+                line_count++;
+            else if(x == ':')
+                colon_count++;
+        }
+        x = fgetc(f);
+    }
+    str[i++] = '\n';
+    str[i] = '\0';
+
+    
+    
+    instructions = malloc(line_count*sizeof(struct instruction));
+    labels = malloc(colon_count*sizeof(struct label));
+
+    
+    // Skipping empty lines
+    x = 0;
+    while(str[x] == '\n')
+    {
+        x++;
+        k++;    // Incrementing the line number
+    }
+    i = x;        // Beginnning of the first non-empty line
+    
+    int j, t;
+    pc = 0;
+    lab_no = 0;
+    while(str[x] != '\0')
+    {
+        if(str[x] == ':')    // to obtain the instruction which the label is identifying
+        {
+            t = x--;        // t stores the position of the colon
+            while(str[x] != ' ' && str[x] != '\t' && str[x] != '\n')
+            {
+                x--;
+                if(x < 0)
+                    break;
+            }
+            labels[lab_no].i = x+1;
+            labels[lab_no].j = t;
+            j = x;
+            x = t+1;
+            while(str[x] == ' ' || str[x] == '\t')
+            {
+                ++x;
+            }
+            if(str[x] == '\n')    // label is pointing to the instruction in the next line
+            {
+                while(j > 0)
+                {
+                    if(str[j] != ' ' && str[j] != '\t')
+                        break;
+                    --j;
+                }
+                if(str[j] == '\n' || j == -1)
+                    labels[lab_no++].inst_no = pc;        // There is no instruction in the line of this label
+                else
+                {
+                    labels[lab_no++].inst_no = pc+1;    // There is an instruction before this label in the same line but label points to the next line
+                    instructions[pc].i = i;
+                    instructions[pc].j = j+1;
+                    instructions[pc].line = k;
+                    pc++;
+                }
+            }
+            else
+            {
+                labels[lab_no++].inst_no = pc;        // Label points to an instruction in the same line
+                instructions[pc].i = x;
+                while(str[x] != '\n')
+                    x++;
+                instructions[pc].j = x;
+                instructions[pc].line = k;
+                pc++;
+            }
+            while(str[x] == '\n')    // Neglecting all blank lines after this label
+            {
+                x++;
+                k++;    // Incrementing the line number
+            }
+            i = x;
+        }
+        else if(str[x] == '\n')            // This is the end of an instruction
+        {
+            j=x;
+            instructions[pc].i = i;
+            instructions[pc].j = j;
+            instructions[pc].line=k;
+            pc++;
+            
+            while(str[x] == '\n')        // Neglecting all blank lines after this instruction
+            {
+                x++;
+                k++;    // Incrementing the line number
+            }
+            i = x;
+        }
+        else
+            x++;
+    }
+    int inst_count = pc;
+    lab_count = lab_no;
+    setPcForMain();
+    reg[14] = 4092;        // setting the stack pointer to 0xFFC (the end of the memory)
+    while(pc < inst_count)
+    {
+        k = instructions[pc].line;    // Line number of the current pc
+        executeInstruction();        // Executes the instruction corresponding to the current pc
+        pc++;
+    }
+    
     return 0;
 }
 
