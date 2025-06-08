70beded46d4b2d3940e6920ac76d964497c29619
evans-prince <princeevans2004@gmail.com>
2025-06-08 11:36:44 +0530

    Declared feilds and methods for emulator

--- CLI-Emulator/main.c
+++ CLI-Emulator/main.c
@@ -1,4 +1,91 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+// reg will contain the value of the sixteen registers r0 - r15
+int reg[16];
+
+// flags contains flags(E) and flags(GT)
+int flags[2];
+
+// Data Memory of 4096 bytes
+int Mem[1024];
+
+// rd stores destination register, rs1 and rs2 store the source registers and imm is the immediate
+int rd, rs1, rs2, imm;
+
+// m stores the modifier, isImm stores whether the instruction contains an immediate
+int m, isImm;
+
+// str is the string containing the whole program
+char* str;
+
+// Executes the instruction depending upon the value of pc
+void executeInstruction(void);
+
+// Stores the register numbers of a 3-address instruction in rd, rs1 and rs2 or imm
+void getReg3Add(char* inst, int i);
+
+// Stores the register numbers of a 2-address instruction in rd and rs2 or imm
+void getReg2Add(char* inst, int i);
+
+// Stores the register numbers of a ld/st instruction in rd, rs1 and imm
+void getLdSt(char* inst, int i);
+
+// Prints the line number where an error has occured and exits the program
+void invalidInst(void);    
+
+// Returns decimal value of a hexadecimal digit
+int dec(char ch);
+
+// sets pc for the main function
+void setPcForMain(void);
+
+// gets pc for a given label str[i,j]
+int getPcForLabel(char* str, int i, int j);
+
+// k stores the line number of the instruction
+int k;
+
+// pc stores the instruction number and therefore pc will be the (program counter)/4
+int pc;
+
+// lab_no stores the label number
+int lab_no;
+
+// lab_count stores the number of labels in the program
+int lab_count;
+
+// An array used to convert a hexadecimal immediate to decimal
+int hexImm[4];
+
+// encodedInst will contain the encoding of an instruction
+unsigned int encodedInst;
+
+// A struct for each instruction
+struct instruction
+{
+    int i;        // starting index of inst
+    int j;      // ending index of inst
+    int line;    // line in which the instruction appears
+};
+
+// An array of all instruction structs of the program (behaves somewhat like the instruction memory)
+struct instruction *instructions;
+
+// A struct for each label
+struct label
+{
+    int i;        // starting index of label
+    int j;        // ending index of label
+    int inst_no;  // inst_no this label points to
+};
+
+// An array of label structs of the program
+struct label *labels;
+
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
